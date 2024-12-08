9db99b53c295555c40f11e09bad6d104842c2bef
yasushinri <1543971140@qq.com>
2024-12-08 18:47:54 +0800

    start

---
+++ test.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Yasushin Ri on 2024/12/8.
+//
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+  cout << "Hello World!" << endl;
+
+
+  return 0;
+}
